f26daa073a94a5992df813b938d27f210d310428
pandrosov <pandrosov@list.ru>
2025-11-15 19:01:14 +0300

    Исправлена последняя ошибка компиляции в auth.ts

--- frontend/src/store/auth.ts
+++ frontend/src/store/auth.ts
@@ -30,7 +30,7 @@ interface AuthState {
   logout: () => void;
 }
 
-export const useAuthStore = create<AuthState>((set, get) => ({
+export const useAuthStore = create<AuthState>((set) => ({
   user: null,
   loading: true,
   error: null,
